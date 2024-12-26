f0a30f8286b07ef3044e1eb6b2e57083e5484168
Parth Antala <theparthantala@gmail.com>
2024-12-26 16:23:04 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Parth Antala on 2024-12-26.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
